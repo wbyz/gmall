f871930f31499c53428280e1aebf14e30de573d9
wbyz <279951831@qq.com>
2019-07-20 09:24:27 +0800

    update url in LogUploader

--- gmall-mock/src/main/java/com/atguigu/bigdata/gmall/mock/util/LogUploader.java
+++ gmall-mock/src/main/java/com/atguigu/bigdata/gmall/mock/util/LogUploader.java
@@ -17,7 +17,8 @@ public class LogUploader {
         try{
             //不同的日志类型对应不同的URL
 
-            URL url  =new URL("http://localhost/log");
+//            URL url  =new URL("http://localhost/log");
+            URL url  =new URL("http://logserver/log");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             //设置请求方式为post
